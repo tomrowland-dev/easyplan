01b65c74141e02d75bc862f731de572048b69f69
tom / <tom.rowland@xavier.vic.edu.au>
2023-06-23 14:26:31 +1000

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace easyplan.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace easyplan.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
