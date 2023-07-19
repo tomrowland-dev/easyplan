f135d8871580964be31bd940c945456152d48169
tom / <tom.rowland@xavier.vic.edu.au>
2023-07-19 23:39:05 +1000

    added todolist stuff

---
+++ Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace easyplan.Models
+{
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
---
+++ Views/ToDoList.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace easyplan.Views;
+
+public partial class ToDoList : UserControl
+{
+    public ToDoList()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
